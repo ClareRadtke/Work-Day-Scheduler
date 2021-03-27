029b47ff8a968fe414908e5f5ac8c4b373ca7f09
Clare Radtke <clare.radtke@outlook.com>
2021-03-27 16:30:09 +1100

    Timeblocks generated via Jquery. Serialization set up, WIP saving events

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,51 @@
 // Display the current day
-var today = moment();
+const today = moment();
 $("#currentDay").text(today.format("dddd, Do MMMM"));
+
+const savedAt = today.format("YYYY-MM-DD");
+// color block past, present and future hours
+// identify current hour moment()
+//if hour
+
+// create the timeblocks
+// delegate JQuery - events to elements pre-exist
+const $container = $(".container");
+
+for (let i = 9; i <= 17; i++) {
+  const hour = i > 12 ? i - 12 : i;
+  const meridiem = i < 12 ? "AM" : "PM";
+
+  $container.append(`
+  <div class="row">
+    <label for="desctiption" class="hour col">${hour} ${meridiem}</label>
+    <textarea id="desctiption" name="desctiption" class="description row col-10" rows="4" cols="33">
+    </textarea>
+    <button type="button" class="saveBtn col"><i class="far fa-save"></i></button>
+  </div>
+  `);
+}
+
+// save any textarea input to local storage textArea.value
+//serialization - JSON.stringify();
+
+const schedulerStorage = window.localStorage;
+const scheduledEvents = {
+  "9AM": "",
+  "10AM": "hai;er 56346ufh",
+  "11AM": "hai;erufh",
+  "12PM": "",
+  "1PM": "",
+  "2PM": "hai;erufh",
+  "3PM": "hai;e67463 rufh",
+  "4PM": "hai;erufh",
+  "5PM": "",
+};
+// Any textarea value is to be stored in an object
+// that is then serialized and stored in localStorage
+
+function storeScheduledEvents(obj) {
+  let serializeScheduledEvents = JSON.stringify(obj);
+  schedulerStorage.setItem(savedAt, serializeScheduledEvents);
+}
+
+// .click(saveBtn)
